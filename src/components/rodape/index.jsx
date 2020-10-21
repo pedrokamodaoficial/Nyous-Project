e9f98e438fc6a7fc89966030b4d3118aa57544ba
pedrokamoda04@gmail.com <pedrokamoda04@gmail.com>
2020-10-21 17:44:44 -0300

    Home e Rodape melhorados

--- src/components/rodape/index.jsx
+++ src/components/rodape/index.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 const Rodape = () => {
     return(
         <footer className="text-center" style={{marginTop : '70px'}}>
-            <h1>SENAI Informática</h1>
-            <small>Desenvolvido por <strong>João Pedro</strong></small>
+            <h1>SENAI Informática - Cyberpunk 2077</h1>
+            <small>Desenvolvido por <strong>Pedro Kamoda</strong></small>
         </footer>
     );
 }
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -37,7 +37,7 @@ const Home = () => {
                                 Some quick example text to build on the card title and make up the bulk of
                                 the card's content.
                                 </Card.Text>
-                                <Button variant="primary">Go somewhere</Button>
+                                <Button variant="primary">Ir para</Button>
                             </Card.Body>
                         </Card>
                     </Col>
@@ -50,7 +50,7 @@ const Home = () => {
                                 Some quick example text to build on the card title and make up the bulk of
                                 the card's content.
                                 </Card.Text>
-                                <Button variant="primary">Go somewhere</Button>
+                                <Button variant="primary">Ir para</Button>
                             </Card.Body>
                         </Card>
                     </Col>
@@ -63,7 +63,7 @@ const Home = () => {
                                 Some quick example text to build on the card title and make up the bulk of
                                 the card's content.
                                 </Card.Text>
-                                <Button variant="primary">Go somewhere</Button>
+                                <Button variant="primary">Ir para</Button>
                             </Card.Body>
                         </Card>
                     </Col>
